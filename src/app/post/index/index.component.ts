752eda061ba8e151c1ee7a5a6351793395132dba
mouna jmai <mouna.jmai@gmail.com>
2024-05-14 14:37:39 +0100

    first commit

---
+++ src/app/post/index/index.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { PostService } from '../post.service';
+import { Post } from '../post';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+@Component({
+selector: 'app-index',
+standalone: true,
+imports: [CommonModule,FormsModule,ReactiveFormsModule, RouterModule],
+templateUrl: './index.component.html',
+styleUrl: './index.component.css'
+})
+export class IndexComponent {
+posts: Post[] = [];
+/*------------------------------------------
+--------------------------------------------
+Created constructor
+--------------------------------------------
+--------------------------------------------*/
+constructor(public postService: PostService) { }
+/**
+* Write code on Method
+*
+* @return response()
+*/
+ngOnInit(): void {
+this.postService.getAll().subscribe((data:
+Post[])=>{
+this.posts = data;
+console.log(this.posts);
+})
+}
+/**
+* Write code on Method
+*
+* @return response()
+*/
+deletePost(id:number){
+this.postService.delete(id).subscribe(res => {
+this.posts = this.posts.filter(item => item.id
+!== id);
+console.log('Post deleted successfully!');
+})
+}
+}
